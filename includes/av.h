7025e84b95d9d7b53e11031f370d83798976874d
Niv <dindibo4@gmail.com>
2020-05-28 15:59:42 +0300

    Added option to write progress to file (WIP)

--- includes/av.h
+++ includes/av.h
@@ -1,4 +1,5 @@
 #include "sortedTree.h"
+#include <stdio.h>
 
 #ifndef INCLUDE_AV_H
 #define INCLUDE_AV_H
@@ -13,6 +14,20 @@ struct av
     char sigLoad;
 }typedef av;
 
+enum FILETYPE{
+    FILETYPE_ERR,
+    FILETYPE_REG,
+    FILETYPE_DIR,
+};
+
+struct scanResults
+{
+    int   success;
+    int   threatsFound;
+    int   filetype;
+    char* pathScanned;
+}typedef scanResults;
+
 void av_Init(av *self, char *signaturesPath);
 
 int av_LoadSignatures(av *self);
@@ -25,5 +40,9 @@ void av_SearchViruses(av *self, char *path);
 int av_CheckFile(sortedTree *hashTree, char *path);
 
 void av_AddMalware(av *self, char *path);
+
+scanResults av_SearchViruses_S(av *self, char *filePath);
+
+void av_saveToFile(av *self);
 
 #endif
--- main.c
+++ main.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <wait.h>
+#include <time.h>
+
 
 #include "includes/binode.h"
 #include "includes/sortedTree.h"
@@ -27,6 +32,13 @@ DEST--;\
 }\
 while (!(DEST >= START && DEST <= END));
 
+#define PRINT_RED     printf("\033[0;31m"); 
+#define PRINT_RED_B   printf("\033[1;31m"); 
+#define PRINT_GREEN   printf("\033[0;32m"); 
+#define PRINT_GREEN_B printf("\033[1;32m");
+#define PRINT_RESET   printf("\033[0m");
+#define PRINT_BLUE    printf("\033[0;34m"); 
+
 #define HASH_SIZE 32
 
 #define BUFFER_SIZE_TINY 64
@@ -34,14 +46,6 @@ while (!(DEST >= START && DEST <= END));
 #define BUFFER_SIZE_MEDIUM 256
 #define BUFFER_SIZE_BIG 512
 
-// Globals
-
-av *AV;
-const char *SELECTION_HEADERS[] = {"Scan a file or directory", "Exit"};
-const char *SELECTION_SCAN_HEADERS[] = {"Directory (recursively)", "File", "Exit"};
-void option_scan();
-void option_exit();
-const void(*SELECTION_HANDLERS[])() = {option_scan, option_exit};
 
 // Enums
 
@@ -55,6 +59,51 @@ enum SELECTION_SCAN{
     SCAN_FILE,
     SCAN_EXIT,
 } typedef SELECTION_SCAN;
+
+
+// Structs
+
+struct lastResult
+{
+    scanResults result;
+    char        canOverride;
+}typedef lastResult;
+
+// Globals
+
+void show_menu();
+int fds[2];
+lastResult lResult;
+pid_t pid;
+av *AV;
+const char *SELECTION_HEADERS[] = {"Scan a file or directory", "Exit"};
+const char *SELECTION_SCAN_HEADERS[] = {"Directory (recursively)", "File", "Exit"};
+pid_t option_scan();
+void option_exit();
+void sigHandler(int signo);
+const void(*SELECTION_HANDLERS[])() = {option_scan, option_exit};
+
+static void initLastResult(){
+    lResult.canOverride = 1;
+    memset(&(lResult.result), 0, sizeof(lResult.result));
+}
+
+static void updateLastResult(scanResults res){
+    volatile int a = 0;
+
+    while(lResult.canOverride != 1){
+        a++;
+        a--;
+    }
+
+    lResult.canOverride = 0;
+    memcpy(&(lResult.result), &res, sizeof(res));
+}
+
+static void releaseLastResult(){
+    free(lResult.result.pathScanned);
+    lResult.canOverride = 1;
+}
 
 char sortedTree_tree_funcCompare(void *left, void *right){
     int *nLeft = (int *)left;
@@ -70,11 +119,16 @@ void init(){
         exit(1);
     }
 
+    initLastResult();
+
     AV = (av *)malloc(sizeof(av));
     av_Init(AV, "./sigs");
 
-    if(av_LoadSignatures(AV) > 0)
-        fprintf(stderr, "[+] %d Signatures loaded\n", AV->hashTree->count);
+    if(av_LoadSignatures(AV) > 0){
+        PRINT_GREEN
+        fprintf(stdout, "[+] %d Signatures loaded\n", AV->hashTree->count);
+        PRINT_RESET
+    }
     else{
         puts("LoadSignatures: Unable to load signatures");
         exit(1);
@@ -139,29 +193,166 @@ void start_scan(char *path, SELECTION_SCAN type, int fileIndex, int argc){
     }
 }
 
-void option_scan(){
-    int select;
+pid_t option_scan(){
+    /*int  select;
+    char filePath[BUFFER_SIZE_SMALL];
+    char absoluteFilePath[BUFFER_SIZE_SMALL];
+
     PROMPT_HEADERS(select, SCAN_DIRECTORY, SCAN_EXIT, SELECTION_SCAN_HEADERS)
+
+    switch(select){
+        case SCAN_DIRECTORY:
+        {
+            puts("Enter directory path:");
+
+            break;
+        }
+        case SCAN_FILE:
+        {
+            puts("Enter file path:");
+        }
+        default:
+        {
+            return;
+        }
+    }
+
+    getc(stdin);
+    fgets(filePath, BUFFER_SIZE_SMALL, stdin);
+    realpath(filePath, absoluteFilePath); 
+    removeLastCharecter(absoluteFilePath, '/');
+
+    // If file exists
+    if(1==2 && access(absoluteFilePath, F_OK) == -1){
+        puts("No such file or directory");
+    }
+    // TODO: Check what happens if file is dir
+    else{
+        
+        if(select == SCAN_FILE){
+            char isVirus = av_CheckFile(AV->hashTree, absoluteFilePath);
+            
+            if(isVirus == 1){
+                av_AddMalware(AV, absoluteFilePath);
+            }
+        }
+        else{
+            av_SearchViruses(AV, absoluteFilePath);
+        }
+
+    }*/
+
+    pid_t scanPID;
+    scanResults result;
+    char fileName[BUFFER_SIZE_SMALL];
+    show_menu();
+    fgets(fileName, BUFFER_SIZE_SMALL, stdin);
+    printf("Starting scan at %s", fileName);
+
+    pipe(fds);
+    scanPID = fork();
+
+    if(scanPID == 0){
+        result = av_SearchViruses_S(AV, fileName);
+        int  pathLength = strlen(result.pathScanned);
+        av_saveToFile(AV);
+
+        write(fds[1], &result, sizeof(result));
+        write(fds[1], &pathLength, sizeof(pathLength));
+        write(fds[1], result.pathScanned, pathLength);
+        fflush(NULL);
+
+        free(result.pathScanned);
+        exit(result.success == 1 ? 0 : 1);
+    }
+    else{
+        return scanPID;
+    }
+
 }
 
 void option_exit(){
     exit(0);
 }
 
+void show_menu(){
+    PRINT_BLUE
+    putc(10, stdout);
+    printf("scan>\nEnter file or directory : \n");
+    PRINT_RESET
+}
+
 int main(int argc, char *argv[]){
     init();
+    /*pid_t pid;
 
     int select;
     do
     {
         PROMPT_HEADERS(select, SCAN, EXIT, SELECTION_HEADERS)
-        SELECTION_HANDLERS[select]();
+        //SELECTION_HANDLERS[select]();
+        int result = -1;
+        pid = fork();
+
+        // If child
+        if(pid == 0){
+            result = fake();
+            exit(0);
+        }
+        else{
+            printf("%d\n", result);
+        }
     }
-    while(select != EXIT);
+    while(select != EXIT);*/
 
-    //start_scan(path, type, fileIndex, argc);
-
-    // TODO: Fork - search
+    while(1){
+        pid = option_scan();
+        signal(SIGCHLD, sigHandler);
+    }
 
     return 0;
 }
+
+void sigHandler(int signo){
+    if(signo == SIGCHLD){
+        scanResults result;
+        int  a;
+        int  pathLength;
+        char *pathStr;
+
+        pid_t finished = wait(&a);
+
+        read(fds[0],&result, sizeof(result));
+        read(fds[0],&pathLength, sizeof(pathLength) + 1);
+
+        if(!(pathLength > 0 && pathLength < 129)){
+            exit(1);
+        }
+        
+        pathStr = (char*)malloc(pathLength + 1);
+        read(fds[0],pathStr, pathLength);
+        pathStr[pathLength] = '\x00';
+        
+        result.pathScanned = pathStr;
+
+        updateLastResult(result);
+        printf("finished scan from: %s\n", lResult.result.pathScanned);
+        printf("threats found: %d\n", lResult.result.threatsFound);
+        releaseLastResult();
+
+        // Problematic!
+        if(finished == pid){
+            if(a == 0){
+                puts("Scan complete");
+            }
+            else{
+                puts("Scan failed");
+            }
+        }
+
+        show_menu();
+        printf("%d\n", AV->threatsFound);
+    }
+}
+
+// https://stackoverflow.com/questions/49581349/how-to-get-return-value-from-child-process-to-parent
