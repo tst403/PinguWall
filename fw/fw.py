704ae9d094ca32ba14e8d3208aba3d3d0cf9a9ac
Your Name <you@example.com>
2020-06-15 17:09:25 +0300

    WIP: Added router class

--- fw/fw.py
+++ fw/fw.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python
 
+from scapy.all import *
+
 import nat
 import router
+
+def foo(x):
+    print 'Yes'
+    return True
 
 def get_mac():
     eth = Ether()
     return eth[Ether].src
 
-n = nat.NAT(nat.NAT.FilterType.BLACK_LIST)
-r = router.Router()
+#n = nat.NAT(nat.NAT.FilterType.BLACK_LIST)
 
+mac = get_mac()
+
+r = router.Router(mac, '52:54:00:12:35:02', foo)
+
+r.run()
 
--- fw/router.py
+++ fw/router.py
@@ -1,38 +1,71 @@
 from threading import Thread
 from time import sleep
+from scapy.all import *
+
+
+def get_packet_layers(packet):
+    counter = 0
+    while True:
+        layer = packet.getlayer(counter)
+        if layer is None:
+            break
+
+        yield layer
+        counter += 1
+
+
+def get_layer_after(packet, layerName):
+    getNext = False
+
+    for layer in get_packet_layers(packet):
+        if getNext:
+            return layer
+
+        if layer.name == layerName:
+            getNext = True
+    
+    return None
+
 
 class Router(Thread):
     def __init__(self, mac_addr, default_gateway, handler):
-        super().__init__()
+        super(Thread, self).__init__()
 
         self.mac_addr = mac_addr
         self.default_gateway = default_gateway
         self.handler = handler
     
+
     def _handler_warper(self, pack):
-        if self.route(pack):
-            self.handler(pack)
-            # TODO: handler
+        if self.handler(pack):
+            return self.route(pack)
+
 
     def route(self, pack):
         # If frame was sent to us
-        if pack.has_layer(Ether):
+        if pack.haslayer(Ether):
             if pack[Ether].dst == self.mac_addr:
-                pack[Ether].src = self.mac_addr
-                pack[Ether].dst = self.default_gateway
+                newPack = Ether(src=self.mac_addr, dst=self.default_gateway)
 
-                # if frame has ip layer
-                if pack.has_layer(IP):
-                    pack[IP].ttl -= 1
-                    
-                    if pack[IP].ttl <= 0:
+                # If frame has ip layer
+                if pack.haslayer(IP):
+
+                    newPack /= IP(src='192.168.2.1', dst=pack[IP].dst, ttl=pack[IP].ttl - 1)
+
+                    if newPack[IP].ttl <= 0:
                         print('TTL Error')
                         return False
-                    else:
-                        del pack[IP].chksum
+
+                    # Load packet after IP layer
+                    newPack /= get_layer_after(pack, 'IP')
+
+                    newPack.show()
                     
-                    sendp(pack)
+                    sendp(newPack)
                     return True
-        return True
 
                     
+    return False
+
+    def run(self):
+        sniff(filter='ip', prn=self._handler_warper)
