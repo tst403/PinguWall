ae6ef3499c5186bf5e0fa41f3151bfef81e0f5aa
Niv <dindibo4@gmail.com>
2020-07-24 00:16:32 +0300

    Added feature to save and load configuration

--- fw/patch/moduleBuilder.py
+++ fw/patch/moduleBuilder.py
@@ -3,6 +3,11 @@
 import netutils as net
 import json
 
+getIpByIname = lambda iname : os.popen('ifconfig ' + iname + ' | grep inet | awk \'{$1=$1;print}\' | head -n1 | cut -d \' \' -f2 | tr -d \'\\n\'').read()
+
+getMacByIname = lambda iname : os.popen('ifconfig ' + iname + ' | grep -i ether | awk \'{$1=$1;print}\' | cut -d \' \' -f2 | tr -d \'\\n\'').read()
+
+
 def buildLanNic(name):
     return net.LanNIC(getMacByIname(name), getIpByIname(name), name)
 
@@ -14,6 +19,7 @@ class moduleBuilder:
     def __init__(self, conf_name):
         self.conf_name = conf_name
         
+        print(os.popen('pwd').read())
         with open(self.conf_name, 'r') as f:
             self.jsonContent = json.loads(f.read())
 
@@ -24,4 +30,13 @@ class moduleBuilder:
     def buildWan(self):
         global buildWanNic
         return buildWanNic(self.jsonContent['wanIface'])
+
+    def buildIPPoolLan(self):
+        return self.jsonContent['lanIpMin'], self.jsonContent['lanIpNetmask']
+
+    def buildIPPoolWan(self):
+        return self.jsonContent['wanIpMin'], self.jsonContent['wanIpNetmask']
+
+    def buildDefaultGatewayWan(self):
+        return self.jsonContent['defualtGateway']
 
--- fw/patch/script.py
+++ fw/patch/script.py
@@ -15,17 +15,13 @@ def elevate():
     uid = os.getuid() 
 
     if uid != 0:
-        os.execvp('sudo', ['sudo', exe, *cmd])
-
-getIpByIname = lambda iname : os.popen('ifconfig ' + iname + ' | grep inet | awk \'{$1=$1;print}\' | head -n1 | cut -d \' \' -f2 | tr -d \'\\n\'').read()
-getMacByIname = lambda iname : os.popen('ifconfig ' + iname + ' | grep -i ether | awk \'{$1=$1;print}\' | cut -d \' \' -f2 | tr -d \'\\n\'').read()
-    
+        os.execvp('sudo', ['sudo', exe, *cmd])    
 
 
 mb = moduleBuilder.moduleBuilder('nat.conf')
 
-lan = mb.buildLan() #buildLanNic('enp0s3')
-wan = mb.buildWan() #buildWanNic('enp0s8')
+lan = mb.buildLan()
+wan = mb.buildWan()
 wan.lanNIC = lan
 lan.wanNIC = wan
 
@@ -36,15 +32,12 @@ routeTableWan = net.RoutingTable()
 
 ipPoolOne, ipPoolTwo = mb.buildIPPoolLan(), mb.buildIPPoolWan()
 
-#routeTableLan.add_route(net.IPRoute(net.IPPool('192.168.73.0', '255.255.255.0'), lan.ip_address))
 routeTableLan.add_route(net.IPRoute(net.IPPool(ipPoolOne[0], ipPoolOne[1]), lan.ip_address))
-#routeTableWan.add_route(net.IPRoute(net.IPPool('10.0.3.2', '255.255.255.0'), wan.ip_address))
 routeTableWan.add_route(net.IPRoute(net.IPPool(ipPoolTwo[0], ipPoolTwo[1]), wan.ip_address))
 
 lan.routing_table = routeTableLan
 wan.routing_table = routeTableWan
 
-#wan.routing_table.set_default_gateway('10.0.0.2')
 wan.routing_table.set_default_gateway(mb.buildDefaultGatewayWan())
 lan.routing_table.set_default_gateway(lan.ip_address)
 
